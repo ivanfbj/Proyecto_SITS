b6a3115b6bee01a489e69e49aa3e60b25910e55b
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2021-09-12 23:06:49 -0500

    Comentarios del procedimiento almacenado

--- Sentencias_para_base_de_datos/3-stprInsertarMovimientoProducto.sql
+++ Sentencias_para_base_de_datos/3-stprInsertarMovimientoProducto.sql
@@ -31,21 +31,47 @@ BEGIN
 
 
   BEGIN TRY
-
+	--SI el tipo de movimiento ingresado en el procedimiento almacenado existe comenzará a realizar las validaciones.
     IF EXISTS (SELECT Id FROM tblTipoMovimiento WHERE descripcion = @tipoMovimiento_Descripcion)
 		BEGIN
 		  IF @tipoMovimiento_Id = 1
 			  BEGIN
-				INSERT INTO tblProducto (codigoBarras, nombre, cantidad, precio, fechaCreacion)
-				  VALUES (@codigoBarras, @nombre, @cantidad, @precio, @fechaCreacion);
+				--SI el producto ingresado existe actualizará la cantidad en la tabla "tblProducto"
+				--Insertará el registro en la tabla "tblMovimientoProducto"
+				IF EXISTS (SELECT ID FROM tblProducto WHERE codigoBarras = @codigoBarras)
+					BEGIN
+						DECLARE @producto_id int;
+						
+						SELECT @producto_id = Id 
+							FROM tblProducto 
+							WHERE codigoBarras = @codigoBarras;
 
-				DECLARE @idProductoInsertado int = SCOPE_IDENTITY();
+						UPDATE tblProducto 
+							SET cantidad = cantidad + @cantidad
+						FROM tblProducto
+						WHERE ID = @producto_id
+
+						INSERT INTO tblMovimientoProducto (producto_Id, tipoMovimiento_Id, cantidad, fechaMovimiento)
+							VALUES (@producto_id, @tipoMovimiento_Id,@cantidad ,@fechaCreacion);
+
+
+					END
+				--Como el producto ingresado NO existe, se creará el registro en la tabla "tblProducto"
+				--Insertará el registro en la tabla "tblMovimientoProducto"
+				ELSE
+					BEGIN
+						INSERT INTO tblProducto (codigoBarras, nombre, cantidad, precio, fechaCreacion)
+							VALUES (@codigoBarras, @nombre, @cantidad, @precio, @fechaCreacion);
+
+						DECLARE @idProductoInsertado int = SCOPE_IDENTITY();
 				
-				insert into tblMovimientoProducto (producto_Id, tipoMovimiento_Id,cantidad,fechaMovimiento)
-					values (@idProductoInsertado, @tipoMovimiento_Id,@cantidad ,@fechaCreacion)
+						INSERT INTO tblMovimientoProducto (producto_Id, tipoMovimiento_Id,cantidad,fechaMovimiento)
+							VALUES (@idProductoInsertado, @tipoMovimiento_Id,@cantidad ,@fechaCreacion)
 
 				SELECT
-				  'Ejecucion correcta ' AS 'stpr';
+				  'PRODUCTO CREADO EXITOSAMENTE ' AS 'stpr';
+					END
+
 			  END
 		  ELSE IF @tipoMovimiento_Id = 2
 			  BEGIN
