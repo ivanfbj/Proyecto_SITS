4ef88006044b867eb278008361f2b5837ab1722c
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2021-09-17 18:08:11 -0500

    Insertar productos por base de datos

---
+++ Sentencias_para_base_de_datos/6-Execute stprInsertarMovimientoProducto.sql
@@ -0,0 +1,3 @@
+EXECUTE [dbo].[stprInsertarMovimientoProducto] '7708118955757','Chocolate Colombiano 80% Cacao',2,4500,'Entrada'
+EXECUTE [dbo].[stprInsertarMovimientoProducto] '7702993028261','gomitas Trululu Gusanos ácidos',1,2500,'Entrada'
+EXECUTE [dbo].[stprInsertarMovimientoProducto] '037466023427','Dark Chocolate',4,5600,'Entrada'
