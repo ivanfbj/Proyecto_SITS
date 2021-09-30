4602078a9d6bc7ffb189f233b74da6cc9196415d
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2021-09-29 20:46:17 -0500

    Crear tablas para las otras entidades de la aplicación
    Tablas:
    - tblCombo
    - tblproductoxCombo
    - tblPedido
    - tblproductoxPedido
    
    Co-Authored-By: lfdaach <73004711+lfdaach@users.noreply.github.com>

--- Sentencias_para_base_de_datos/1-Create_tables.sql
+++ Sentencias_para_base_de_datos/1-Create_tables.sql
@@ -26,8 +26,50 @@ CREATE TABLE [dbo].[tblMovimientoProducto]
  
 )
 
+CREATE TABLE [dbo].[tblCombo]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [nroCombo] NVARCHAR(50) NOT NULL unique, 
+    [nombre] NVARCHAR(255) NOT NULL,  
+    [subtotal] MONEY NOT NULL, 
+)
+
+CREATE TABLE [dbo].[tblproductoxCombo]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [combo_Id] INT NOT NULL, 
+    [producto_Id] INT NOT NULL, 
+	CONSTRAINT FK_tblCombo_Id_tblproductoxCombo_combo_Id FOREIGN KEY (combo_Id) REFERENCES tblCombo (Id),
+	CONSTRAINT FK_tblProducto_Id_tblproductoxCombo_producto_Id FOREIGN KEY (producto_Id) REFERENCES tblProducto (Id)
+)
+CREATE TABLE [dbo].[tblPedido]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [nroPedido] NVARCHAR(50) NOT NULL unique, 
+    [nombreCombo] NVARCHAR(50) NOT NULL unique,
+    [fechaPedido] DATETIME NOT NULL DEFAULT Getdate(),
+    [envio] BIT NOT NULL, 
+    [valorEnvio] MONEY NOT NULL,
+    [subtotal] MONEY NOT NULL, 
+    [total] MONEY NOT NULL,
+
+)
+
+CREATE TABLE [dbo].[tblproductoxPedido]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [pedido_Id] INT NOT NULL, 
+    [producto_Id] INT NOT NULL, 
+	CONSTRAINT FK_tblPedido_Id_tblproductoxPedido_pedido_Id FOREIGN KEY (pedido_Id) REFERENCES tblPedido (Id),
+	CONSTRAINT FK_tblProducto_Id_tblproductoxPedido_producto_Id FOREIGN KEY (producto_Id) REFERENCES tblProducto (Id)
+)
+
 /*
 Drop table tblMovimientoProducto
 Drop table tblProducto
 Drop table tblTipoMovimiento
+Drop table tblproductoxPedido
+Drop table tblPedido
+Drop table tblproductoxCombo
+Drop table tblCombo
 */
