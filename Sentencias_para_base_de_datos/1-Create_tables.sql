0369680bcef98920e87997ade21b7ef58a14fea7
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2021-10-23 00:53:52 -0500

    Organizar restricciones de las tablas con lo aprendido
    El objetivo fue ponerle nombre a las PK, UNIQUE y CHECK para un mejor orden y administración.

--- Sentencias_para_base_de_datos/1-Create_tables.sql
+++ Sentencias_para_base_de_datos/1-Create_tables.sql
@@ -1,64 +1,104 @@
-﻿CREATE TABLE [dbo].[tblTipoMovimiento]
+﻿
+CREATE TABLE [dbo].[tblTipoMovimiento]
 (
-	[Id] INT NOT NULL PRIMARY KEY identity, 
-    [descripcion] NVARCHAR(50) not NULL unique
-)
+  [id]          INT NOT NULL IDENTITY, 
+  [descripcion] NVARCHAR(50) NOT NULL,
+  CONSTRAINT [PK_tblTipoMovimiento_id] PRIMARY KEY (id),
+  CONSTRAINT [UQ_tblTipoMovimiento_descripcion] UNIQUE ([descripcion])
+);
 
 CREATE TABLE [dbo].[tblProducto]
 (
-	[Id] INT NOT NULL PRIMARY KEY identity, 
-    [codigoBarras] NVARCHAR(50) NOT NULL unique, 
-    [nombre] NVARCHAR(255) NOT NULL, 
-    [cantidad] INT NOT NULL, 
-    [precio] MONEY NOT NULL, 
-    [fechaCreacion] DATETIME NOT NULL DEFAULT Getdate() 
-)
+  [id]            INT NOT NULL IDENTITY, 
+  [codigoBarras]  NVARCHAR(50) NOT NULL, 
+  [nombre]        NVARCHAR(255) NOT NULL, 
+  [cantidad]      INT NOT NULL, 
+  [precio]        MONEY NOT NULL, 
+  [fechaCreacion] DATETIME NOT NULL
+					  DEFAULT GETDATE(),
+  CONSTRAINT [PK_tblProducto_id] PRIMARY KEY ([id]),
+  CONSTRAINT [UQ_tblProducto_codigoBarras] UNIQUE([codigoBarras]),
+  CONSTRAINT [UQ_tblProducto_nombre] UNIQUE([nombre]),
+  CONSTRAINT [CK_tblProducto_cantidad] CHECK([cantidad] >= 0)
+);
+
+ALTER TABLE [dbo].[tblProducto]
+ADD CONSTRAINT [CK_tblProducto_precio] CHECK([precio] >= 0);
 
 CREATE TABLE [dbo].[tblMovimientoProducto]
 (
-	[Id] INT NOT NULL PRIMARY KEY identity, 
-    [producto_Id] INT NOT NULL, 
-    [tipoMovimiento_Id] INT NOT NULL,
-    [cantidad] INT NOT NULL,
-    [fechaMovimiento] DATETIME NOT NULL DEFAULT Getdate(),
-	CONSTRAINT FK_tblProducto_Id_tblMovimientoProducto_producto_Id FOREIGN KEY (producto_Id) REFERENCES tblProducto (Id),
-	CONSTRAINT FK_tblTipoMovimiento_Id_tblMovimientoProducto_tipoMovimiento_Id FOREIGN KEY (tipoMovimiento_Id) REFERENCES tblTipoMovimiento (Id)
- 
-)
+  [id]                INT NOT NULL IDENTITY, 
+  [producto_Id]       INT NOT NULL, 
+  [tipoMovimiento_Id] INT NOT NULL, 
+  [cantidad]          INT NOT NULL, 
+  [fechaMovimiento]   DATETIME NOT NULL
+						 DEFAULT GETDATE(),
+  CONSTRAINT [PK_tblMovimientoProducto_id] PRIMARY KEY ([ID]),
+  CONSTRAINT [FK_tblProducto_Id_tblMovimientoProducto_producto_Id] FOREIGN KEY([producto_Id]) REFERENCES [tblProducto]([Id]), 
+  CONSTRAINT [FK_tblTipoMovimiento_Id_tblMovimientoProducto_tipoMovimiento_Id] FOREIGN KEY([tipoMovimiento_Id]) REFERENCES [tblTipoMovimiento]([Id]));
+
+ALTER TABLE [dbo].[tblMovimientoProducto] ADD CONSTRAINT [CK_tblMovimientoProducto_precio] CHECK([cantidad] >= 0);
 
 CREATE TABLE [dbo].[tblCombo]
 (
-	[Id] INT NOT NULL PRIMARY KEY identity, 
-    [nroCombo] NVARCHAR(50) NOT NULL unique, 
-    [nombre] NVARCHAR(255) NOT NULL,  
-    [subtotal] MONEY NOT NULL, 
-)
+  [id]       INT NOT NULL IDENTITY, 
+  [nroCombo] NVARCHAR(50) NOT NULL, 
+  [nombre]   NVARCHAR(255) NOT NULL, 
+  [subtotal] MONEY NOT NULL,
+  CONSTRAINT [PK_tblCombo_id] PRIMARY KEY([id]),
+  CONSTRAINT [UQ_tblCombo_nroCombo] UNIQUE([nroCombo])
+);
 
-CREATE TABLE [dbo].[tblproductoxCombo]
+CREATE TABLE [dbo].[tblProductoxCombo]
 (
-	[Id] INT NOT NULL PRIMARY KEY identity, 
-    [combo_Id] INT NOT NULL, 
-    [producto_Id] INT NOT NULL, 
-	CONSTRAINT FK_tblCombo_Id_tblproductoxCombo_combo_Id FOREIGN KEY (combo_Id) REFERENCES tblCombo (Id),
-	CONSTRAINT FK_tblProducto_Id_tblproductoxCombo_producto_Id FOREIGN KEY (producto_Id) REFERENCES tblProducto (Id)
-)
+  [id]          INT NOT NULL IDENTITY, 
+  [combo_id]    INT NOT NULL, 
+  [producto_id] INT NOT NULL,
+  CONSTRAINT [PK_tblProductoxCombo_id] PRIMARY KEY ([id]),
+  CONSTRAINT [FK_tblCombo_Id_tblproductoxCombo_combo_Id] FOREIGN KEY([combo_Id]) REFERENCES [tblCombo]([Id]), 
+  CONSTRAINT [FK_tblProducto_Id_tblproductoxCombo_producto_Id] FOREIGN KEY([producto_Id]) REFERENCES [tblProducto]([Id])
+);
+
 CREATE TABLE [dbo].[tblPedido]
 (
-	[Id] INT NOT NULL PRIMARY KEY identity, 
-    [nroPedido] NVARCHAR(50) NOT NULL unique, 
-    [nombreCombo] NVARCHAR(50) NOT NULL unique,
-    [fechaPedido] DATETIME NOT NULL DEFAULT Getdate()
-    [envio] BIT NOT NULL, 
-    [valorEnvio] MONEY NOT NULL,
-    [subtotal] MONEY NOT NULL, 
-    [total] MONEY NOT NULL,
+  [id]          INT NOT NULL IDENTITY, 
+  [nroPedido]   NVARCHAR(50) NOT NULL, 
+  [nombreCombo] NVARCHAR(50) NOT NULL, 
+  [fechaPedido] DATETIME NOT NULL
+					DEFAULT GETDATE(), 
+  [envio]       BIT NOT NULL, 
+  [valorEnvio]  MONEY NOT NULL, 
+  [subtotal]    MONEY NOT NULL, 
+  [total]       MONEY NOT NULL,
+  CONSTRAINT [PK_tblPedido_id] PRIMARY KEY([id]),
+  CONSTRAINT [UQ_tblPedido_nroPedido] UNIQUE([nroPedido]),
+  CONSTRAINT [UQ_tblPedido_nombreCombo] UNIQUE([nombreCombo]),
+);
 
-)
-CREATE TABLE [dbo].[tblproductoxPedido]
+CREATE TABLE [dbo].[tblProductoxPedido]
 (
-	[Id] INT NOT NULL PRIMARY KEY identity, 
-    [pedido_Id] INT NOT NULL, 
-    [producto_Id] INT NOT NULL, 
-	CONSTRAINT FK_tblPedido_Id_tblproductoxPedido_pedido_Id FOREIGN KEY (pedido_Id) REFERENCES tblPedido (Id),
-	CONSTRAINT FK_tblProducto_Id_tblproductoxPedido_producto_Id FOREIGN KEY (producto_Id) REFERENCES tblProducto (Id)
-)
+  [id]          INT NOT NULL IDENTITY, 
+  [pedido_id]   INT NOT NULL, 
+  [producto_id] INT NOT NULL,
+  CONSTRAINT [PK_tblProductoxPedido_id] PRIMARY KEY(id),
+  CONSTRAINT [FK_tblPedido_id_tblproductoxPedido_pedido_id] FOREIGN KEY([pedido_id]) REFERENCES [tblPedido]([id]), 
+  CONSTRAINT [FK_tblProducto_id_tblproductoxPedido_producto_id] FOREIGN KEY([producto_id]) REFERENCES [tblProducto]([id]));
+/*
+Drop table tblproductoxPedido
+Drop table tblPedido
+Drop table tblproductoxCombo
+Drop table tblCombo
+Drop table tblMovimientoProducto
+Drop table tblProducto
+Drop table tblTipoMovimiento
+*/
+/*
+SELECT CONSTRAINT_NAME,
+     TABLE_SCHEMA ,
+     TABLE_NAME,
+     CONSTRAINT_TYPE
+     FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+   WHERE TABLE_NAME in ('tblMovimientoProducto','tblProducto','tblTipoMovimiento','tblproductoxPedido','tblPedido','tblproductoxCombo','tblCombo')
+   order by 3
+*/
+
